0399f8049056c35e7f82810e727901c8679355af
Sam Barskiy <63865780+SamBarskiy@users.noreply.github.com>
2025-11-10 16:08:05 -0600

    Add files via upload

--- locomotion.py
+++ locomotion.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-#from sensor_msgs.msg import LaserScan
-#from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import LaserScan
+from rclpy.qos import qos_profile_sensor_data
 from geometry_msgs.msg import Twist
 from rclpy.qos import QoSProfile
 import time
@@ -12,8 +12,19 @@ class Locomotion(Node):
 
     def __init__(self):
         super().__init__('turtlebot3_locomotion')
-        self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.subscription_scan = self.create_subscription(LaserScan, '/base_scan', self.sensor_callback, 20)
+        # self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        # self.subscription_scan = self.create_subscription(LaserScan, '/base_scan', self.sensor_callback, 20)
+
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/front_state',
+            self.listener_callback,
+            10)
+        qos = QoSProfile(depth=10)
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 1)
+        self.subscription  # prevent unused variable warning
+
+
         self.timer = self.create_timer(0.01, self.timer_callback)
 
         # Set baseline numbers
@@ -29,17 +40,27 @@ class Locomotion(Node):
         self.left_distance = float('inf')
         self.right_distance = float('inf')
 
+        # Multipliers to make conversion easier
+        self.distance_multiplier = 3
+        self.linear_speed_multiplier = 3
+        self.angular_speed_multiplier = 3
+
         self.move_cmd = Twist()
 
     # Sensor callback
-    def sensor_callback(self, msg: LaserScan):
+    def listener_callback(self, msg: LaserScan):
 
         twist = Twist()
 
         def filter_vals(range):
             range = [v for v in range if not math.isinf(v) and not math.isnan(v)]
             return min(range) if range else float('inf')
-
+        arr = msg.ranges[-135:]+msg.ranges[:-135]
+        msg.ranges = arr[0:271]
+        msg.ranges = [5.0 if x == 0.0 else (x*self.distance_multiplier) for x in msg.ranges]
+        # self.get_logger().info(msg.ranges)
+        # self.get_logger().info(msg.ranges)
+        # self.get_logger().info(msg.crash)
         n = len(msg.ranges)
         front = n // 2
         right = n // 6
@@ -58,19 +79,20 @@ class Locomotion(Node):
         match self.state:
             # base state is following a wall on right-hand side
             case "following-wall":
-                twist.linear.x = 0.5
+                self.get_logger().info("Following wall")
+                twist.linear.x = 0.5 * self.linear_speed_multiplier
                 twist.angular.z = 0.0
 
                 # Course correct to stay parallel
                 # Drifting away, turn slightly right toward wall 
                 if(msg.ranges[right-1] < msg.ranges[right+1]):
-                    twist.angular.z = -0.1
+                    twist.angular.z = -0.1 * self.angular_speed_multiplier
                 # Drifting toward, turn slightly left away from wall
                 if(msg.ranges[right-1] > msg.ranges[right+1]):
-                    twist.angular.z = 0.1
+                    twist.angular.z = 0.1 * self.angular_speed_multiplier
                 # Anti-crash mechanism - don't hug the wall too close
                 if(self.right_distance < 0.3):
-                    twist.angular.z = 0.2
+                    twist.angular.z = 0.2 * self.angular_speed_multiplier
 
                 # if the front value gets too low, we are in a corner and need to turn left
                 if(self.front_distance < 0.8):
@@ -78,8 +100,9 @@ class Locomotion(Node):
 
             # turn right for a little bit, then inch forward into doorway
             case "right-turn":
+                self.get_logger().info("Right turn")
                 twist.linear.x = 0.0
-                twist.angular.z = -1.0
+                twist.angular.z = -1.0 * self.angular_speed_multiplier
                 self.turn_timer += 1
 
                 # if we are aligned with a wall mid-turn, stop turning 
@@ -89,7 +112,7 @@ class Locomotion(Node):
 
                 # done turning 90 degrees, so inch forward
                 if(self.turn_timer > 19 and self.turn_timer < 24):
-                    twist.linear.x = 0.15
+                    twist.linear.x = 0.15 * self.linear_speed_multiplier
                     twist.angular.z = 0.0
 
                 if(self.turn_timer >= 24):
@@ -98,17 +121,19 @@ class Locomotion(Node):
 
             # turn left until the area in front is clear
             case "left-turn":
+                self.get_logger().info("left turn")
                 twist.linear.x = 0.0
-                twist.angular.z = 1.0
+                twist.angular.z = 1.0 * self.angular_speed_multiplier
 
                 if(self.front_distance > 1.0):
-                    twist.linear.x = 0.1
+                    twist.linear.x = 0.1 * self.linear_speed_multiplier
                     twist.angular.z = 0.0
                     self.state = "following-wall"
 
             # check to see if the doorway is wide enough for the robot to fit
             case "checking-right-doorway":
-                twist.linear.x = 0.2
+                self.get_logger().info("checking right doorway")
+                twist.linear.x = 0.2 * self.linear_speed_multiplier
                 twist.angular.z = 0.0
                 self.doorway_timer += 1
 
@@ -126,7 +151,7 @@ class Locomotion(Node):
         self.previous_right = self.right_distance
         self.previous_left = self.left_distance
         
-        self.cmd_pub.publish(twist)
+        self.pub.publish(twist)
 
     # Timer callback
     def timer_callback(self):
